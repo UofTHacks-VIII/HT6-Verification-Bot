ddc5a14fee29d18fb009f0dcd8d9b6971ec42797
Henry T <henry@henrytu.me>
2020-08-28 14:49:15 -0400

    Invite script

---
+++ src/scripts/add_to_discord.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+// Nice env code by Alex @ Hack the 6ix
+const env_data = fs.readFileSync('../data/.env', {encoding: 'utf8'}).split(/\r?\n/)
+
+const env = {};
+env_data.forEach((line) => {
+  if (line.length > 0) {
+    const lineArray = line.split('=');
+    const key = lineArray.splice(0, 1);
+
+    env[key] = lineArray.join('=').replace(/\"/g, '');
+  }
+});
+
+if(mongoose.connect(env.DB_CONN, { useNewUrlParser: true })) {
+  console.log("DB Connected")
+}
+
+const discordSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  discordID: String,
+  discordTag: String,
+  displayName: String,
+  roles: [String],
+  timeOfCheckin: Number
+});
+
+const discords = mongoose.model('discords', discordSchema);
+
+fs.readFile('users.json', 'utf8', async (err, data) => {
+
+  const jsonData = JSON.parse(data);
+
+  console.log(data);
+
+  for (let user of jsonData) {
+    try {
+      const result = await discords.create(user);
+      console.log('Success', result)
+    } catch (e) {
+      console.log('Error', e, user);
+    }
+  }
+});
---
+++ src/scripts/parse_csv.py
@@ -0,0 +1,45 @@
+import csv
+import json
+
+data = []
+
+valid_roles = [
+  'attendee',
+  'verified',
+  'instructor'
+]
+
+with open('deets.csv', 'r') as file:
+  reader = csv.reader(file)
+
+  for row in reader:
+    company = row[0].strip()
+    name = row[1].strip()
+    email = row[2].lower()
+    roles = row[3:]
+
+    formatted_roles = []
+
+    for r in roles:
+      if r:
+        formatted_roles.append(r)
+
+        if r not in valid_roles:
+          raise Exception('%s is not a valid role!' % r)
+          break
+
+    if company.strip():
+      display_name = '%s (%s)' % (name.split()[0], company)
+    else:
+      display_name = name.strip()
+
+    data.append({
+      'displayName': display_name,
+      'email': email,
+      'roles': formatted_roles
+    })
+
+print(data)
+
+with open('users.json', 'w') as file:
+  json.dump(data, file, indent=4)
