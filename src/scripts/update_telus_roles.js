e74f2dda3722abb98adebda2a119a3e623adafc4
Henry T <henry@henrytu.me>
2021-02-17 14:17:18 -0500

    Telus

---
+++ src/scripts/update_telus_roles.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+// Nice env code by Alex @ Hack the 6ix
+const env_data = fs.readFileSync('../data/.env', {encoding: 'utf8'}).split(/\r?\n/)
+
+const env = {};
+env_data.forEach((line) => {
+  if (line.length > 0) {
+    const lineArray = line.split('=');
+    const key = lineArray.splice(0, 1);
+
+    env[key] = lineArray.join('=').replace(/\"/g, '');
+  }
+});
+
+if(mongoose.connect(env.DB_CONN, { useNewUrlParser: true })) {
+  console.log("DB Connected")
+}
+
+const discordSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  discordID: String,
+  discordTag: String,
+  displayName: String,
+  roles: [String],
+  timeOfCheckin: Number,
+  expires: Number
+});
+
+const discords = mongoose.model('discords', discordSchema);
+
+discords.updateMany({
+  displayName:/.*Telus*./,
+  expires: null
+}, {
+  roles: [
+      "verified",
+      "sponsor-mentor",
+      "mentor"
+  ]
+}, (err, user) => {
+  console.log(err, user);
+});
